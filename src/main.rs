d9ec1836938fa06230f0ced1c3db4aa20343108f
Alexander Dmitriev <alexander.dmitriev@yahoo.com>
2024-09-16 19:28:55 +0400

    Базовый http-server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, web::resource, App, HttpResponse, HttpServer, Responder};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(resource("/").to(|| async { "Example server" })))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
